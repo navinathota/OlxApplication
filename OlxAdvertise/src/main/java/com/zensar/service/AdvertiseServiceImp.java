d0d8f1f232d873c7382ead486a34839746f73867
navinathota <102963902+navinathota@users.noreply.github.com>
2022-05-06 19:02:34 +0530

    Update AdvertiseServiceImp.java

--- OlxAdvertise/src/main/java/com/zensar/service/AdvertiseServiceImp.java
+++ OlxAdvertise/src/main/java/com/zensar/service/AdvertiseServiceImp.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.zensar.entity.Advertisement;
-@Service
+@Service 
 public class AdvertiseServiceImp implements AdvertiseService{
 	static List<Advertisement> advertisement = new ArrayList<Advertisement>();
 	//public List<Advertisement> advertises=new ArrayList<Advertisement>();
