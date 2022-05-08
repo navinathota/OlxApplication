2d5820947098ad29cff79a067efd294012eb829e
navinathota <102963902+navinathota@users.noreply.github.com>
2022-05-09 02:10:55 +0530

    Delete OlxLogin directory

--- OlxLogin/src/main/java/com/zensar/login/endpoint/CustomEndpoint.java
+++
@@ -1,5 +0,0 @@
-package com.zensar.login.endpoint;
-
-public class CustomEndpoint {
-
-}
--- OlxLogin/src/main/java/com/zensar/login/repository/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.login.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.login.entity.User;
-
-public interface LoginRepository extends JpaRepository<User,Long>{
-
-}
